d0fe2f49bfb195bd9a34ba25dbc88c95ac94ba38
PrathikGopal <djprathik@protonmail.com>
2026-07-21 03:04:07 +0530

    Fix: remove stale PID files in start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# Start D-Bus (Required for Avahi to broadcast over mDNS)
+# Remove stale PID files that might block services from starting
+rm -f /var/run/dbus/pid
+rm -f /var/run/avahi-daemon/pid
+
+# Start D-Bus
 mkdir -p /var/run/dbus
 dbus-daemon --system
 
-# Start Avahi Daemon (AirPrint discovery)
+# Start Avahi Daemon
 avahi-daemon -D
 
-# Start AirSane in the background (eSCL scanning server)
+# Start AirSane in the background
 airsaned &
 
-# Start CUPS server in the foreground to keep the container running
+# Start CUPS server in the foreground
 exec /usr/sbin/cupsd -f
